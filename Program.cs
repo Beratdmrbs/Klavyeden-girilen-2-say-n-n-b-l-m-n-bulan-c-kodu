470b9be59689ff2f1ae9544fb21c33aa599b4d6f
Beratdmrbs <beratdmrbs61@gmail.com>
2024-10-21 21:50:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int bolunen, bolen, bolum;
+
+Console.WriteLine("Lütfen Bölünen sayıyı giriniz");
+bolunen = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Lütfen Bölen sayıyı giriniz");
+bolen = Int32.Parse(Console.ReadLine());
+
+bolum = bolunen / bolen;
+
+Console.WriteLine("Çıkan bölüm: {0}", bolum);
+Console.ReadLine();
